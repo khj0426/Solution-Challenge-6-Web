e67b0055357f23634526ac8eee153732c61179de
Kim0426 <706shin1728@naver.com>
2023-02-18 08:58:48 +0900

    feat:자동 로그인 추가

--- src/api/getAccessToken.ts
+++ src/api/getAccessToken.ts
@@ -1,11 +1,36 @@
 import axios from 'axios';
-const getAccessToken = ({ username }: { username: string }) => {
-  const getToken = async () => {
-    const res = await axios.post('', {});
+const getAccessToken = ({
+  username,
+  email,
+  password,
+}: {
+  username: string;
+  email: string;
+  password: string;
+}) => {
+  const getLoginToken = async () => {
+    const res = await axios.post('http://34.82.77.224/login', {
+      email: email,
+      password: password,
+    });
+    console.log(res);
     return res;
   };
+  const getSignInToken = async () => {
+    const res = await axios.post('http://34.82.77.224/login/google', {
+      email,
+      name: username,
+      password,
+    });
+    if (res.status === 200) {
+      localStorage.setItem('accessToken', res.data.accessToken);
+    }
+  };
 
-  return getToken();
+  if (localStorage.getItem('accessToken') && true) {
+    return getLoginToken();
+  }
+  return getSignInToken();
 };
 
 export default getAccessToken;
--- src/components/Logout/SignoutGoogle.tsx
+++ src/components/Logout/SignoutGoogle.tsx
@@ -1,19 +1,15 @@
 import { auth } from '../../api/firebase';
 import { signOut } from 'firebase/auth';
-import { useDispatch } from 'react-redux';
-import * as actions from '../Store/module/user';
 import { useRouter } from 'next/router';
 import { Button } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import * as actions from '../Store/module/user';
 
 const SignOutGoogle = () => {
   const router = useRouter();
   const dispatch = useDispatch();
   const handleSingout = async () => {
     await signOut(auth).then(() => {
-      sessionStorage.removeItem('accessToken');
-      sessionStorage.removeItem('refreshToken');
-      sessionStorage.removeItem('persist:root');
-      sessionStorage.removeItem('imgURL');
       dispatch(actions.LOGOUTCHECK());
     });
   };

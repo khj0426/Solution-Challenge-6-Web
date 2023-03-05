6c8199e6e19a46497772b91cb1b103d044206d0b
Kim0426 <706shin1728@naver.com>
2023-03-05 19:23:26 +0900

    feat:미션 성공 시 나타날 컴포넌트 개발

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        destination: 'https://bepserver.duckdns.org/main',
+        source: '/api/main',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
---
+++ src/components/Mission/Success.tsx
@@ -0,0 +1,118 @@
+//미션 성공시 나타날 컴포넌트
+import styled, { css } from 'styled-components';
+import Image from 'next/image';
+export const MissonSuccess = () => {
+  return (
+    <>
+      <StyledSection>
+        <Image
+          style={{ objectFit: 'cover', display: 'block' }}
+          src="/img/success.png"
+          alt="misson success image"
+          height={300}
+          width={350}
+        />
+
+        <StyledP>LIFE BELOW WATER</StyledP>
+        <StyledH2>Find the Country with the Effel Tower.</StyledH2>
+        <StylerdTextArea>
+          France, in Western Europe, encompasses medieval cities, alpine
+          villages and Mediterranean beaches. Paris, its capital, is famed for
+          its fashion houses, classical art museums including the Louvre and
+          monuments like the Eiffel Tower. The country is also renowned for its
+          wines and sophisticated cuisine. Lascaux’s ancient cave drawings,
+          Lyon’s Roman theater and the vast Palace of Versailles attest to its
+          rich history. France, in Western Europe, encompasses medieval cities,
+          alpine villages and Mediterranean beaches. Paris, its capital, is
+          famed for its fashion houses, classical art museums including the
+          Louvre and monuments like the Eiffel Tower. The country is also
+          renowned for its wines and sophisticated cuisine. Lascaux’s ancient
+          cave drawings, Lyon’s Roman theater and the vast Palace of Versailles
+          attest to its rich history.
+        </StylerdTextArea>
+
+        <StyledButton>
+          <Image
+            src="/img/donation 1.png"
+            alt="dantate image"
+            width={30}
+            height={30}
+          />
+          Donate
+        </StyledButton>
+      </StyledSection>
+    </>
+  );
+};
+
+const StyledButton = styled.button`
+  background: #367bb7;
+  display: flex;
+  justify-content: center;
+  gap: 3px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 40px;
+  width: 300px;
+  font-weight: 700;
+  margin-top: 30px;
+  height: 45px;
+  align-items: center;
+  color: #ffffff;
+`;
+
+const StyledP = styled.p`
+  font-size: 13px;
+  display: flex;
+  width: 100%;
+  font-weight: 700;
+  margin-top: 30px;
+  color: #616161;
+`;
+
+const StylerdTextArea = styled.div`
+  color: #555555;
+  margin-top: 20px;
+  display: flex;
+  max-height: 400px;
+  justify-content: center;
+  align-items: center;
+  overflow-y: scroll;
+  flex-wrap: wrap;
+  &::-webkit-scrollbar {
+    border-radius: 6px;
+    width: 12px;
+    background-color: rgba(255, 255, 255, 0.4);
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.3);
+    border-radius: 6px;
+  }
+`;
+
+const StyledH2 = styled.h2`
+  font-weight: 700;
+  display: flex;
+  width: 100%;
+`;
+
+const StyledSection = styled.section`
+  margin: 0 auto;
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  width: 350px;
+  height: 600px;
+  border-radius: 5px 0px 0px 5px;
+  background-color: #dedede;
+
+  @media (max-width: 500px) {
+    width: 350px;
+    height: auto;
+    max-height: 80%;
+  }
+`;

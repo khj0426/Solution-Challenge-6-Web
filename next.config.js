30247c07031fe186c1b30e5659996dab42a0e7e6
Kim0426 <706shin1728@naver.com>
2023-03-09 14:35:19 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   async rewrites() {
     return [
       {
-        destination: 'https://bepserver.duckdns.org/main/:path*',
         source: '/api/main/:path*',
-        
-        destination: 'https://bepserver.duckdns.org/main',
+        destination: 'https://bepserver.duckdns.org/main/:path*',
+      },
+      {
         source: '/api/main',
+        destination: 'https://bepserver.duckdns.org/main',
       },
     ];
   },

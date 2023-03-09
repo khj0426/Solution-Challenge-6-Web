e647227341b93935a2b32a5cb88b91674b4a52a9
Kim0426 <706shin1728@naver.com>
2023-03-09 14:27:40 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
       {
         destination: 'https://bepserver.duckdns.org/main/:path*',
         source: '/api/main/:path*',
+        
+        destination: 'https://bepserver.duckdns.org/main/',
+        source: '/api/main/,
       },
     ];
   },
